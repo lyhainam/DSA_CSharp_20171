e7e1ed1b79572fd06cb33e4b5f9b5d3ca40df6d8
Ly Hai Nam <hainamlycb080597@gmail.com>
2018-06-25 16:39:31 +0700

    Đếm số từ trong xâu

---
+++ DemTu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemTu
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] s;
+            char[] c = { '\t', ' ', '`', '~', '!', '@', '#',
+                           '$', '%', '^', '&', '*', '(', ')',
+                           '-', '_', '=', '+', '[', ']', '{',
+                           '}', ';', '\'', '\\', ':', '\"',
+                           '|', ',', '.', '/', '<', '>', '?' };
+            Console.Write("Enter a string: ");
+            s = Console.ReadLine().Split(c, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("The string you have entered has {0} word(s)", s.Length);
+        }
+    }
+}
---
+++ DemTu_ver2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DemTu_ver2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s;
+            int i = 0, count = 1;
+            Console.Write("Enter a string: ");
+            s = Console.ReadLine();
+
+            while (i <= s.Length - 1)
+            {
+                if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+                {
+                    count++;
+                }
+                i++;
+            }
+            Console.WriteLine("The string you have entered has {0} word(s)", count);
+        }
+    }
+}
