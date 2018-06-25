adefcebc6036f370ee2f10ed8f7d9666d29d251e
Ly Hai Nam <hainamlycb080597@gmail.com>
2018-06-25 13:56:54 +0700

    Giải phương trình bậc hai trên trường số phức

---
+++ ComplexQuadratic/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Complex
+{
+    class Complex
+    {
+        public double real;
+        public double imaginary;
+
+        public Complex(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        // Declare which operator to overload (+)
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex(c1.real + c2.real, c1.imaginary + c2.imaginary);
+        }
+
+        // Declare which operator to overload (-)
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex(c1.real - c2.real, c1.imaginary - c2.imaginary);
+        }
+
+        // Declare which operator to overload (*)
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            return new Complex(c1.real * c2.real - c1.imaginary * c2.imaginary, c1.real * c2.imaginary + c1.imaginary * c2.real);
+        }
+
+        // Declare which operator to overload (/)
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            return new Complex((c1.real * c2.real + c1.imaginary * c2.imaginary) / (c2.real * c2.real + c2.imaginary * c2.imaginary), (c1.imaginary * c2.real - c1.real * c2.imaginary) / (c2.real * c2.real + c2.imaginary * c2.imaginary));
+        }
+
+        // Declare which method to take the square root of a complex number
+        public static Complex Sqrt(Complex c1)
+        {
+            double x, y;
+            x = Math.Sqrt((c1.real + Math.Sqrt(c1.real * c1.real + c1.imaginary * c1.imaginary)) / 2);
+            y = Math.Sqrt((-c1.real + Math.Sqrt(c1.real * c1.real + c1.imaginary * c1.imaginary)) / 2);
+            return new Complex(x, y);
+        }
+
+        // Override the ToString method to display an complex number in the suitable format:
+        public override string ToString()
+        {
+            return (String.Format("{0} + {1}i", real, imaginary));
+        }
+    }
+
+    class ComplexQuadraticEquation
+    {
+        static void Main()
+        {
+            // Enter input data
+            // Enter 'a' constant
+            Console.Write("Enter the real of a: ");
+            double a1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the imaginary of a: ");
+            double a2 = double.Parse(Console.ReadLine());
+            Complex a = new Complex(a1, a2);
+            Console.WriteLine("The 1st complex number as 'a' constant is:  {0}\n", a);
+
+            // Enter 'b' constant
+            Console.Write("Enter the real of b: ");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the imaginary of b: ");
+            double b2 = double.Parse(Console.ReadLine());
+            Complex b = new Complex(b1, b2);
+            Console.WriteLine("The 2nd complex number as 'b' constant is:  {0}\n", b);
+
+            // Enter 'c' constant
+            Console.Write("Enter the real of c: ");
+            double c1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the imaginary of c: ");
+            double c2 = double.Parse(Console.ReadLine());
+            Complex c = new Complex(c1, c2);
+            Console.WriteLine("The 3rd complex number as 'c' constant is:  {0}", c);
+
+            // Save B = -b / 2 to computing the Delta formula
+            Complex B = new Complex(-b1 / 2, -b2 / 2);
+
+            // Computing the Delta formula
+            Complex Delta = B * B - a * c;
+
+            // Taking the Square root of the Delta formula
+            Complex Omega = Complex.Sqrt(Delta);
+
+            // Solving both of the formula
+            Complex z1 = (B + Omega) / a;
+            Complex z2 = (B - Omega) / a;
+
+            // The problem and solution below:
+            Console.WriteLine("\n");
+            Console.WriteLine("The quadratic equation need to solve: ({0})z*z + ({1})z + ({2}) = 0", a, b, c);
+            Console.WriteLine("\n");
+            Console.WriteLine("The quadratic formulas of this equation are:\nz1 = " + z1 + "\n" + "z2 = " + z2 + "\n");
+        }
+    }
+}
---
+++ Complex_Quadratic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace Complex_Quadratic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nNhap phan thuc so phuc a: ");
+            double a1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nNhap phan ao so phuc a: ");
+            double a2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nNhap phan thuc so phuc b: ");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nNhap phan ao so phuc b: ");
+            double b2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nNhap phan thuc so phuc c: ");
+            double c1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nNhap phan ao so phuc c: ");
+            double c2 = double.Parse(Console.ReadLine());
+            Complex a = new Complex(a1, a2);
+            Complex b = new Complex(b1, b2);
+            Complex c = new Complex(c1, c2);
+            Console.WriteLine("\nPhuong trinh bac hai co dang: ({0} + {1})z^2 + ({2} + {3}i)z + ({4} + {5}i) = 0", a1, a2, b1, b2, c1, c2);
+            Complex Delta = b * b - 4 * a * c;
+            if (Delta == 0)
+            {
+                Console.WriteLine("\nPhuong trinh co nghiem kep la: z = {0} + {1}i", (-b / a).Real, (-b / a).Imaginary);
+            }
+            else
+            {
+                Complex Omega = Complex.Sqrt(Delta);
+                Complex z1 = (-b + Omega) / 2 / a;
+                Complex z2 = (-b - Omega) / 2 / a;
+                Console.WriteLine("\nPhuong trinh co hai nghiem phan biet la:\nz1 = {0} + {1}i va\nz2 = {2} + {3}i", z1.Real, z1.Imaginary, z2.Real, z2.Imaginary);
+            }
+        }
+    }
+}
